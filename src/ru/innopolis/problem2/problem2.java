caca9c1ed76eb7509fa7b055091849984bbe832c
G-Akleh <98668965+G-Akleh@users.noreply.github.com>
2022-04-06 16:59:03 +0300

    Update problem2.java

--- src/ru/innopolis/problem2/problem2.java
+++ src/ru/innopolis/problem2/problem2.java
@@ -1,2 +1,17 @@
 // your solution here
 //fix all the errors including package errors
+public interface LightningPort {
+
+    void chargeByLightning();
+
+}
+
+public class ChargerStation {
+
+    void chargeByLightning(LightningPort lightningPort){
+        lightningPort.chargeByLightning();
+    }
+}
+
+
+
